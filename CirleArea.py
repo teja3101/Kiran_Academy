655b131f775f820479106edb400f9bd8371f81e5
teja3101 <tejudhanawade31@gmail.com>
2025-11-29 01:25:23 +0530

    First Commit

---
+++ CirleArea.py
@@ -0,0 +1,8 @@
+# Accept the data from terminal
+radius = float(input("Enter the Radius of the Circle: "))
+
+# Calculate the area of circle
+area = 3.14 * radius * radius
+
+# Print the calculated area of circle
+print("Area of Circle is: ", area)
---
+++ eval.py
@@ -0,0 +1,34 @@
+# e1 = "10+20+5//2*5/5"
+# print(e1)
+# print(type(e1))
+
+# res = eval(e1)
+# print(res)
+# print(type(res))
+
+# a = input("Enter any value: ")
+# b = "25"
+# c = "True"
+# d = "Tejaswini"
+
+# r1 = eval(a)
+# print(r1)
+# print(type(r1))
+
+# r2 = eval(b)
+# print(r2)
+# print(type(r2))
+
+# r3 = eval(c)
+# print(r3)
+# print(type(r3))
+
+# r4 = eval("d")
+# print(r4)
+# print(type(r4))            #give double "" for str 
+
+num1 = int(input("Enter any value: "))
+print(num1 * 5)
+
+num2 = eval(input("Enter any value: "))
+print(num2 * 2)
