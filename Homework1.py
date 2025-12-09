13687c1feebefaa3a561ae852efa24eb4f25dc08
teja3101 <tejudhanawade31@gmail.com>
2025-12-09 19:12:02 +0530

    Homework1

---
+++ Homework1.py
@@ -0,0 +1,31 @@
+r = "Tejaswini Vishnu Dhanawade"
+
+print(r[0]+r[1]+r[2]+r[3]+r[4])                   #Tejas
+
+print(r[-26]+r[-25]+r[-24]+r[-23]+r[-22])         #Tejas
+
+print(r[-22]+r[-23]+r[-24]+r[-25]+r[-26])         #sajeT
+
+print(r[10]+r[11]+r[12]+r[13]+r[14]+r[15])        #Vishnu
+
+print(r[-11]+r[-12]+r[-13]+r[-14]+r[-15]+r[-16])     #unhsiV
+
+print(r[10]+r[11]+r[12]+r[13]+r[14]+r[15]+r[16]+r[17]+r[18]+r[19]+r[20]+r[21]+r[22]+r[23]+r[24]+r[25])        #Vishnu Dhanawade
+
+print(r[17]+r[18]+r[19]+r[20]+r[21]+r[22]+r[23]+r[24]+r[25])     #Dhanawade
+
+print(r[-9]+r[-8]+r[-7]+r[-6]+r[-5]+r[-4]+r[-3]+r[-2]+r[-1])     #Dhanawade
+
+print(r[5]+r[6]+r[7]+r[8]+r[9]+r[10]+r[11]+r[12]+r[13])          #wini Vish
+
+print(r[-21]+r[-20]+r[-19]+r[-18]+r[-17]+r[-16]+r[-15]+r[-14]+r[-13])          #wini Vish
+
+print(r[-26]+r[-25]+r[-24]+r[-23]+r[-22]+r[-21]+r[-20]+r[-19]+r[-18])          #Tejaswini
+
+print(r[0]+r[1]+r[2]+r[3]+r[4]+r[5]+r[6]+r[7]+r[8])              #Tejaswini
+
+print(r[0]+r[1]+r[2]+r[3]+r[4]+r[9]+r[10]+r[11]+r[12]+r[13]+r[16]+r[17]+r[18]+r[19]+r[20])           #Tejas Vish Dhan
+
+print(r[12]+r[13]+r[14]+r[15])     #shnu
+
+print(r[-14]+r[-13]+r[-12]+r[-11])     #shnu
